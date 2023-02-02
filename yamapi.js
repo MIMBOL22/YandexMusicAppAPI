c8cc9c9806619f7a2d7176bd582cfc4b8002531e
MIMBOL228 <aekemerov@yandex.ru>
2023-02-02 15:20:53 +0500

    Chaining. yamapi.js

--- yamapi.js
+++ yamapi.js
@@ -3,7 +3,9 @@ import fetch from 'node-fetch';
 import osLib from 'os';
 const yandexPath = "C:/Users/" + osLib.userInfo().username + "/AppData/Local/Packages/A025C540.Yandex.Music_vfvw9svesycw6/LocalCache/Logs",
     // Да, да, этот путь у всех статичный.
-    apiLink = "https://api.music.yandex.net/tracks/";
+
+    apiLink = "https://api.music.yandex.net/tracks/",
+    regexMetaMusic = /(\d+)\: (.*) ~ (.*)/;
 
 const yamapi = {
     getSong: async() => {
@@ -11,7 +13,6 @@ const yamapi = {
             findInFiles.find("PlayTrackInternalAsync", yandexPath, /log.*\.txt/)
                 .then(rows => {
                     let lastRow = rows[Object.keys(rows)[0]];
-                    let regexMetaMusic = /(\d+)\: (.*) ~ (.*)/gm;
                     let metaMusicFromApp = JSON.parse(lastRow.line[lastRow.count - 1]).Track;
                     let metaMusic = regexMetaMusic.exec(metaMusicFromApp);
                     let song = {
@@ -19,6 +20,8 @@ const yamapi = {
                         author: metaMusic[2],
                         name: metaMusic[3]
                     };
+                    return song;
+                }).then(song => {
                     fetch(apiLink + song.id)
                         .then(response => response.json())
                         .then(response => {
